2cb11a047030566ee93e3041925a8af6b04530da
Oladipo Osinubi <diposinubi@gmail.com>
2020-05-18 15:22:08 -0400

    added some css q exit()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import Card from './components/Card';
+import Card from './components/Card'
 import './App.css';
 
 function App() {
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class Card extends React.Component {
-    render (){
+    render() {
         return (
-            <div> This is where my CARD WILL GO</div>
+            <div className='github-profile'>
+                <img src="https://placehold.it/75" />
+                <div className="info">
+                    <div className="name">Name here...</div>
+                    <div className="company">Company here...</div>
+                </div>
+            </div>
         );
     }
 };
