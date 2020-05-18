63da80a26d8832d5feaf0920a3d4acc42361094f
Oladipo Osinubi <diposinubi@gmail.com>
2020-05-18 14:57:17 -0400

    created a card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header title = "The Github Cards App" />
+      <Card />
     </div>
   
   );
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render (){
+        return (
+            <div> This is where my CARD WILL GO</div>
+        );
+    }
+};
+
+
+export default Card;
